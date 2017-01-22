bcfbb89707f73a1f372ff8f2b6dca1e53a3adbc3
Xin00163 <wangxin00163@yahoo.co.uk>
2017-01-22 12:41:58 +0000

    initial version

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,5 +5,15 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
-
-
+-- create database tournament;
+create table players (
+      player_id serial primary key,
+      name text
+);
+create table matches (
+      matches_id serial primary key,
+      winner_id integer,
+      loser_id integer,
+      Foreign key (winner_id) references players(player_id),
+      Foreign key (loser_id) references players(player_id)
+);
